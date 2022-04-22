bc74bf8409490f1beeb124b6cf9a747092b9fff1
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-04-22 11:28:37 +0530

    Update week 2.sql

--- week 2.sql
+++ week 2.sql
@@ -21,3 +21,22 @@ Q4 : sailors id whose rating is 10 or boat id is not 104
         select sid from Sailors where
         rating = 10 or
         sid not in ( select sid from reserves where bid = 104);
+        
+Q5 : sailors whose boat id = 103
+
+        select sname from Sailors where
+        sid in ( select sid from reserves where bid = 103)
+        
+Q6 : sailors who has red boat
+        
+        select sname from Sailors where
+        sid in ( select sid from reserves where
+        bid in ( select bid from Boats where color = "RED"));
+        
+Q7 : sailors who has not red boat
+         
+        select sname from Sailors where
+        sid in ( select sid from reserves where
+        bid not in ( select bid from Boats where color = "RED"));
+        
+  
