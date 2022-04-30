fd8e932df713d311c0d3730095bf0aa1b3cbbc0b
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-04-30 15:00:08 +0530

    Update week - 3 functions.sql

--- week - 3 functions.sql
+++ week - 3 functions.sql
@@ -52,7 +52,7 @@ Q5 : Count the names of different sailor names.
     
 Q6 : Find the names of sailors who are older than the oldest sailor with a rating of 10.
 
-    sname from sailors where
+    select sname from sailors where
     age > (select max(age) from sailors where rating = 10);
     
         +--------+
@@ -62,3 +62,32 @@ Q6 : Find the names of sailors who are older than the oldest sailor with a ratin
         | Lubber |
         | Bob    |
         +--------+
+
+Q7 : Find the age of the youngest sailors for each rating level
+
+    select rating, min(age) from sailors group by rating;
+    
+        +--------+----------+
+        | rating | min(age) |
+        +--------+----------+
+        |      1 |       33 |
+        |      3 |     25.5 |
+        |      7 |       35 |
+        |      8 |     25.5 |
+        |      9 |       35 |
+        |     10 |       16 |
+        +--------+----------+
+ Q8 : Find the age of the youngest sailor who is eligible to vote for each rating level with at least two such sailors. 
+ 
+     select rating, min(age) from sailors group by rating having count(rating) > 1;
+        +--------+----------+
+        | rating | min(age) |
+        +--------+----------+
+        |      3 |     25.5 |
+        |      7 |       35 |
+        |      8 |     25.5 |
+        |     10 |       16 |
+        +--------+----------+
+Q9 : For each red boat, find the number of reservations for this boat.
+  
+  
