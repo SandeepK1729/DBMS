914ab23902317b4130c540e7e0b5f4a4427742d3
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-05-11 11:03:12 +0530

    Create week-4.sql

---
+++ week-4.sql
@@ -0,0 +1,3 @@
+
+mysql> create table Staff_Record(staffid int, name varchar(20), dob date, sex char, salary float, award int, district varchar(20), department varchar(20));
+        Query OK, 0 rows affected (0.01 sec)
