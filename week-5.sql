f3a18a2f381f92a6e31416778b54cf417c84e738
saiganesh08 <93111749+saiganesh08@users.noreply.github.com>
2022-05-18 10:15:17 +0530

    Update week-5.sql

--- week-5.sql
+++ week-5.sql
@@ -1,1 +1,14 @@
 Aim : Working with triggers using QL/SQL
+working with triggers using ql/sql
+A trigger is the set of sql statement stored in the database which are executed or fixed when some event associated with the table occurs.
+Trigger are special kind of storing procedures that are executed atomatically in reponse of some event
+Syntax :
+	create (or) replace trigger <trigger_name>
+		<trigger_time>
+		<trigger_event>
+		on tablename for each row 
+Trigger body: Declare
+		<declare statement>
+	      begin
+		<trigger statement>
+	      end;
