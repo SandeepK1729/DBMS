f24fa8bcf4975d627ec869a7466e2d18c63bb46d
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-05-25 09:26:21 +0530

    Update week-4.sql

--- week-4.sql
+++ week-4.sql
@@ -14,6 +14,22 @@ week - 4 :
         (1009, "Luaretta Tai", "1982-09-23", 'F', 8327.30, 3, "Tai Wai", "Account"),
         (1010, "Gregory tai", "1972-10-22", 'M', 35542.40, 4, "Tai Wo", "Purchase");
 
+        +---------+---------------+------------+------+---------+-------+--------------+------------+
+        | staffid | name          | dob        | sex  | salary  | award | district     | department |
+        +---------+---------------+------------+------+---------+-------+--------------+------------+
+        |    1001 | Jeffrey Lee   | 1978-02-19 | M    | 28463.4 |     3 | Tai Kok Tsui | Sales      |
+        |    1002 | Hugo Cheung   | 1976-04-08 | M    | 14598.5 |     2 | Central      | Sales      |
+        |    1003 | Jennifer Wong | 1978-03-29 | M    |   39850 |     6 | Tai Po       | Sales      |
+        |    1004 | Melinda Ma    | 1982-08-28 | F    |    7783 |     6 | Tai Po       | Purchase   |
+        |    1005 | Hilda Leung   | 1982-10-24 | F    | 45670.5 |     2 | Western      | Sales      |
+        |    1006 | Nelly Tam     | 1973-10-10 | F    | 45670.5 |     4 | Shatin       | Sales      |
+        |    1007 | Mable Mee     | 1979-08-30 | F    |  4530.8 |     1 | Toi Kok Tsui | Purchase   |
+        |    1008 | Barnabv Nge   | 1980-05-12 | M    |  3549.4 |     5 | Hunghom      | Account    |
+        |    1009 | Luaretta Tai  | 1982-09-23 | F    |  8327.3 |     3 | Tai Wai      | Account    |
+        |    1010 | Gregory tai   | 1972-10-22 | M    | 35542.4 |     4 | Tai Wo       | Purchase   |
+        +---------+---------------+------------+------+---------+-------+--------------+------------+
+        10 rows in set (0.01 sec)
+
 Q1 : Write a SQL statement to produce a list of male staff only, showing their names in upper case, department in lower case and the number of characters in the department
 
         select upper(name) as NAME, lower(department) as DEPT, length(department) as len from staff_record where sex = 'M';
